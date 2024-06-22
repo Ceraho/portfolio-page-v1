a2954d62cbc353875a330d53113a49421ef6a377
ecerrahoglu <cerrahoglu.erim@gmail.com>
2024-06-22 20:18:53 +0200

    updated cv file

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 const ResumePage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <a href="/erim_cerrahoglu_cv.pdf" target="_blank">
+      <a href="https://docs.google.com/document/d/e/2PACX-1vQGJZwtvQTAJAOpIqFkB4-3zHl5pr6WJLIGMmfXyKg38WKGxWpEWmv0Rkpcamd9_D0JSI_lNjhRZ6RE/pub" target="_blank">
         <Button variant="getInTouchCTA" size="getInTouchCTASize">
           View Full CV
         </Button>
